f1d013e97d6887b9eba5d3e4e05b68ed57978c0b
mk-pmb <mk-pmb@users.noreply.github.com>
2014-10-09 09:31:34 +0200

    clean-up script

---
+++ clean.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# -*- coding: utf-8, tab-width: 2 -*-
+SELFPATH="$(readlink -m "$0"/..)"
+
+
+function main () {
+  cd "$SELFPATH" || return $?
+  local LITTER=()
+  readarray -t LITTER < <(git status --short | sed -nre 's~^\?\? ~~p')
+  local RM_RV=0
+  if [ -n "${LITTER[*]}" ]; then
+    rm -v "${LITTER[@]}"
+    RM_RV=$?
+  else
+    echo -en '(no litter found)\t'
+  fi
+  ls --all --classify --color=always .
+  return $RM_RV
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+main "$@"; exit $?
